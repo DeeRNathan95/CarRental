3fcdad4a7165b17fb60a0e12a922d0c2f04b8aa4
DeeRNathan95 <dee.rnathan95@gmail.com>
2024-10-10 22:57:22 +0200

    Got the Title, Sub title and the car image and nav and footer set up today.

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="overflow-hidden">
-        <div className="hero">
-          <div className="flex-1 pt-36 padding-x">
-            
-          </div>
-        </div>
+        <Hero />
     </main>
   );
 }
