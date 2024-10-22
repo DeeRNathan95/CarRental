6f5762c086fb29f29c62323007071e02dc1825a0
DeeRNathan95 <d.r.nathan@outlook.com>
2024-10-22 19:31:47 +0200

    setting up API

---
+++ utils/index.ts
@@ -0,0 +1,16 @@
+const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'b9416743b2msh13f2ce799d3d152p149bfcjsnc1ead5d219d0',
+		'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
